da56cafb0f8eac3902c5f25f67b94233dee8d53c
Elizabet Liber <elizabet.liber@gmail.com>
2019-09-10 23:08:11 +0500

    добавила filter по полям

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './App.css';
 import list from './list.js';
 
-console.log(list)
 
 function App() {
+  const [field,setField] = useState('name')
+  const [searchText, setSearchText] = useState('')
+  const onChange = (event) => setSearchText(event.target.value.toLowerCase())
   return (
     <div className="App">
+      <div>active: {field}</div>
+      <input type="text" name="fname" onChange={onChange}/>
       <table style={{width: '100%'}}>
         <tr>
-          <th>name</th>
-          <th>date</th>
-          <th>count</th>
+          <th onClick={() => setField('name')}>name</th>
+          <th onClick={() => setField('date')}>date</th>
+          <th onClick={() => setField('count')}>count</th>
         </tr>
 
         {
-          list.map((element) =>
+          list.filter(element => element[field].toString().toLowerCase().includes(searchText)).map((element) =>
             <tr>
               <td>{element.name}</td>
               <td>{element.date}</td>
