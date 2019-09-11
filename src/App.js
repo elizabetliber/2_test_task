ba4e24c36a9efdde7fa4b125a9f5fb411c951066
Elizabet Liber <elizabet.liber@gmail.com>
2019-09-11 20:54:43 +0500

    добавила сортировку по полям

--- src/App.js
+++ src/App.js
@@ -4,13 +4,37 @@ import list from './list.js';
 
 
 function App() {
-  const [field,setField] = useState('name')
+  const [field,setField] = useState('name');
+  const [sortMode,setSortMode] = useState('asc');
   const [searchText, setSearchText] = useState('')
   const onChange = (event) => setSearchText(event.target.value.toLowerCase())
+  const smartSort = (a, b) => {
+    if (['name', 'date'].includes(field)) {
+      if(a[field] < b[field]) { return -1; }
+      if(a[field] > b[field]) { return 1; }
+      return 0;
+    }
+    if (['count'].includes(field)) {
+      return a[field] - b[field];
+    }
+  };
+
+  let newList = list.slice();
+
+  if (sortMode === 'asc') {
+    newList = list.slice().sort(smartSort)
+  }
+  if (sortMode === 'desc') {
+    newList = list.slice().sort(smartSort).reverse()
+  }
   return (
     <div className="App">
       <div>active: {field}</div>
-      <input type="text" name="fname" onChange={onChange}/>
+      <div>sort: {sortMode}</div>
+      <div onClick={() => setSortMode('asc')}>ASC</div>
+      <div onClick={() => setSortMode('desc')}>DESC</div>
+      <div onClick={() => setSortMode('all')}>ALL</div>
+      <input type="text" name="fname" onChange={onChange} />
       <table style={{width: '100%'}}>
         <tr>
           <th onClick={() => setField('name')}>name</th>
@@ -19,7 +43,7 @@ function App() {
         </tr>
 
         {
-          list.filter(element => element[field].toString().toLowerCase().includes(searchText)).map((element) =>
+          newList.filter(element => element[field].toString().toLowerCase().includes(searchText)).map((element) =>
             <tr>
               <td>{element.name}</td>
               <td>{element.date}</td>
